f783c9d08c51e44236c77102ed4bee89c7724e91
deborahwang <44012229+deborahwang@users.noreply.github.com>
2019-12-06 15:19:26 -0800

    recover "@PreAuthorize("hasRole('ADMIN')")"

--- bank_system/src/main/java/com/hendisantika/onlinebanking/resource/UserResource.java
+++ bank_system/src/main/java/com/hendisantika/onlinebanking/resource/UserResource.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
-//@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserResource {
 
   @Autowired
